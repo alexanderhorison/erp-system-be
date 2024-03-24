778d153ca80cfffbea2d3c765e5f5bfd47bbbeab
farhadyanuar <farhad.yanuar@bithealth.co.id>
2024-03-24 22:41:30 +0700

    [no-ticket] Fixing seeders in menu and role

--- seeders/20240321232336-create_menu.js
+++ seeders/20240321232336-create_menu.js
@@ -5,24 +5,52 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     const menus = [
       {
-        name: "Dashboard",
-        description: "homepage atau dashboard",
+        name: "Pengguna",
+        description: "membuat pengguna dan otoritasnya",
       },
       {
-        name: "Master Data",
-        description: "menu master data",
+        name: "Otoritas Pengguna",
+        description: "membuat otoritas pengguna dan menu yang bisa diakses",
+      },
+      {
+        name: "Kategori Produk",
+        description: "manajemen kategori produk",
+      },
+      {
+        name: "Tipe Produk",
+        description: "manajemen satuan produk",
+      },
+      {
+        name: "Produk",
+        description: "manajemen produk",
+      },
+      {
+        name: "Gudang",
+        description: "manajemen gudang",
+      },
+      {
+        name: "Rumus Transformasi",
+        description: "manajemen rumus untuk mengubah satuan produk",
+      },
+      {
+        name: "List Produk Gudang",
+        description:
+          "melihat produk gudang, set lowstock alert dan transformasi ke satuan lebih kecil",
+      },
+      {
+        name: "Penyesuaian Stok Produk Gudang",
+        description:
+          "melihat produk gudang, menambahkan/mengurangi stok, set low stock alert",
       },
       {
         name: "Surat Jalan",
-        description: "menu surat jalan",
+        description:
+          "list surat jalan beserta status, membuat surat jalan dari warehouse ke warehouse lain",
       },
       {
-        name: "User",
-        description: "menu user & role permission",
-      },
-      {
-        name: "Stock Management",
-        description: "menu stock management",
+        name: "Penerimaan Surat Jalan",
+        description:
+          "list surat jalan pending, menerima produk dari warehouse sumber dan input produk diterima",
       },
     ];
     menus.forEach((menu) => {
@@ -37,11 +65,17 @@ module.exports = {
       "Menus",
       {
         name: [
-          "Dashboard",
-          "Master Data",
+          "Pengguna",
+          "Otoritas Pengguna",
+          "Kategori Produk",
+          "Tipe Produk",
+          "Produk",
+          "Gudang",
+          "Rumus Transformasi",
+          "List Produk Gudang",
+          "Penyesuaian Stok Produk Gudang",
           "Surat Jalan",
-          "User",
-          "Stock Management",
+          "Penerimaan Surat Jalan",
         ],
       },
       {}
--- seeders/20240321232500-create_role.js
+++ seeders/20240321232500-create_role.js
@@ -5,19 +5,53 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     // Fetch all menus
     const menus = await queryInterface.sequelize.query(
-      `SELECT id FROM "Menus";`,
+      `SELECT id, name FROM "Menus";`,
       {
         type: Sequelize.QueryTypes.SELECT,
       }
     );
-    let arrayMenu = menus.map((data) => data.id);
+    let menuAdmin = [];
+    let menuKepalaGudang = [];
+    let menuAdminGudang = [];
+    menus.forEach((data) => {
+      menuAdmin.push(data.id);
+      switch (data.name) {
+        case "List Produk Gudang":
+          menuKepalaGudang.push(data.id);
+          menuAdminGudang.push(data.id);
+          break;
+        case "Penyesuaian Stok Produk Gudang":
+        case "Surat Jalan":
+          menuKepalaGudang.push(data.id);
+          break;
+        case "Penerimaan Surat Jalan":
+          menuAdminGudang.push(data.id);
+          break;
+        default:
+          break;
+      }
+    });
     await queryInterface.bulkInsert(
       "Roles",
       [
         {
           name: "Admin",
           description: "administrator",
-          MenuId: arrayMenu,
+          MenuId: menuAdmin,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Kepala Gudang",
+          description: "kepala gudang",
+          MenuId: menuKepalaGudang,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "Admin Gudang",
+          description: "admin gudang",
+          MenuId: menuAdminGudang,
           createdAt: new Date(),
           updatedAt: new Date(),
         },
@@ -27,6 +61,10 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete("Roles", { name: "Admin" }, {});
+    await queryInterface.bulkDelete(
+      "Roles",
+      { name: ["Admin", "Kepala Gudang", "Admin Gudang"] },
+      {}
+    );
   },
 };
