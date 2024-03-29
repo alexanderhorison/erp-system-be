f56e548f126febd5e1d2cd5a092ce52b5663343c
root <root@tjahayaberkatabadi.com>
2024-03-29 15:53:52 +0000

    commit from server

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const helmet = require("helmet");
 const router = require("./routers");
 const app = express();
-const port = process.env.PORT || 4001;
+const port = process.env.PORT;
 
 app.use(cors());
 // enabling the Helmet middleware
@@ -12,7 +12,7 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", router);
+app.use("/api", router);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
