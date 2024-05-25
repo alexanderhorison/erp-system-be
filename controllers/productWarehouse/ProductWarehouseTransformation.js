e53b9b2d6e62837ed7de521bc5b6fd687bfc6e90
dendijoil <dendijoil@gmail.com>
2024-05-25 21:56:42 +0700

    [IS-38] Transformation Product Warehouse (#10)

---
+++ controllers/productWarehouse/ProductWarehouseTransformation.js
@@ -0,0 +1,60 @@
+const { responses } = require("../../helpers/responses");
+const { yupSchemaValidation } = require("../../helpers/yupSchemaValidation");
+const yup = require("yup");
+
+const ProductWarehouseTransformationService = require("../../services/productWarehouse/ProductWarehouseTransformationService");
+
+class ProductWarehouseTransformation {
+  static async getList(req, res) {
+    try {
+      const schemaParams = yup
+        .number()
+        .required("Id produk warehouse tidak boleh kosong");
+
+      const id = await yupSchemaValidation(req.params.id, schemaParams);
+
+      const data = await ProductWarehouseTransformationService.getAllListTransformationByProductWarehouseId(id)
+
+      res.status(200).json(responses(true, "Success get rumus transformasi produk", data));
+    } catch (error) {
+      return res
+        .status(error.code || 500)
+        .json(responses(false, error.message || error));
+    }
+  }
+
+  static async transformProduct(req, res) {
+    try {
+      const schemaParams = yup
+        .number()
+        .required("Id produk warehouse tidak boleh kosong");
+
+      const schemaBody = yup.object({
+        MasterTransformationId: yup.number().required("Id Transformasi harus di isi"),
+        ProductWarehouseId: yup.number().required("Id produk harus di isi"),
+        qtyTransformation: yup.number().required("Jumlah harus di isi"),
+      })
+
+      const body = await yupSchemaValidation(req.body, schemaBody);
+
+      const id = await yupSchemaValidation(req.params.id, schemaParams);
+
+      const user = req.UserData;
+
+      ProductWarehouseTransformationService.transformProduct({ id: id, data: body, user })
+
+      res
+        .status(200)
+        .json(
+          responses(true, "Produk berhasil ditransformasi", [])
+        );
+
+    } catch (error) {
+      return res
+        .status(error.code || 500)
+        .json(responses(false, error.message || error));
+    }
+  }
+}
+
+module.exports = ProductWarehouseTransformation
--- routers/productWarehouse/index.js
+++ routers/productWarehouse/index.js
@@ -1,11 +1,14 @@
 const router = require("express").Router();
+const routerTransformation = require("./routerTransformation")
+
 const ProductWarehouseController = require("../../controllers/productWarehouse/ProductWarehouseController");
 
+
 router.get("/", ProductWarehouseController.getAllWarehouse);
+router.use("/transformation", routerTransformation)
 router.get("/list", ProductWarehouseController.getListProduct)
 router.get("/warehouse/:id", ProductWarehouseController.getProductByWarehouse);
 router.post("/create/:WarehouseId", ProductWarehouseController.create);
-
 
 router.get("/:id", ProductWarehouseController.getProductWarehouse);
 router.put("/:id", ProductWarehouseController.adjustProduct);
