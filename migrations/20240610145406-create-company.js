7cf1e12b9b01e105f60f15bd88ade4877c81951e
dendijoil <dendijoil@gmail.com>
2024-06-15 13:11:17 +0700

    Update migration company include seed menu

--- migrations/20240610145406-create-company.js
+++ migrations/20240610145406-create-company.js
@@ -24,8 +24,29 @@ module.exports = {
         type: Sequelize.DATE,
       },
     });
+
+    const lastMenu = await queryInterface.sequelize.query(
+      `SELECT id, "menuId" FROM "Menus" ORDER BY id DESC LIMIT 1;`,
+      {
+        type: Sequelize.QueryTypes.SELECT,
+      }
+    );
+
+    await queryInterface.bulkInsert("Menus",
+      [
+        {
+          name: "Company",
+          description: "Manajemen Company Produk",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          menuId: lastMenu[0].menuId + 1
+        }
+      ]
+    )
+
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable("Companies");
+    await queryInterface.bulkDelete("Menus", { name: "Company" }, {});
   },
 };
