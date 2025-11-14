fddf9d238f9f3e50323ec2ac57550a8589c3e1fc
dendijoil <dendijoil@gmail.com>
2025-11-14 13:14:53 +0700

    Add single printer health check and test print functionality to PrinterHealthCheckController

--- routers/healthCheck/index.js
+++ routers/healthCheck/index.js
@@ -3,5 +3,7 @@ const router = require("express").Router();
 const PrinterHealthCheckController = require("../../controllers/healthCheck/printerHealthCheckController");
 
 router.get("/printer", PrinterHealthCheckController.healthCheckAllPrinters);
+router.post("/printer/single", PrinterHealthCheckController.healthCheckSinglePrinter);
+router.post("/printer/test-print", PrinterHealthCheckController.testPrint);
 
 module.exports = router;
